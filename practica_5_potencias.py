f470378567847c8e5ec1646ef4d5047fcc077611
Aldo-Zavala <a23110123@ceti.mx>
2025-09-16 17:00:02 -0600

    Practica_5

---
+++ practica_5_potencias.py
@@ -0,0 +1,5 @@
+#En esta practica vamos a elevar un numero a auna potencai utilizando el factor de potencia "**"
+multiplicacion = 3*3*3*3*3
+potencia = 3**5
+print("el resultado de la multiplicacion 3*3*3*3*3 es : ",multiplicacion)
+print("el resultado de 3^5 utilizando 3**5 es: ",potencia)
