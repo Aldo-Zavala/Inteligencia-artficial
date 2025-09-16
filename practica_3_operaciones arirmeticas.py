1c1ee7af7a22891c9cf664b9f3f3cab628f35b59
Aldo-Zavala <a23110123@ceti.mx>
2025-09-16 16:37:58 -0600

    avances

---
+++ practica_3_operaciones arirmeticas.py
@@ -0,0 +1,20 @@
+#declaramos las variables dato_1 y dato_2 con numeros
+dato_1 = 10 
+dato_2 = 2
+
+#sumamos los dos valores 
+suma = dato_1 + dato_2
+#restamos los dos valores de las dos formas
+resta_1 = dato_1 - dato_2
+resta_2 = dato_2 - dato_1
+#multiplicamos los dos valores
+multiplicacion = dato_1 * dato_2
+#dividimos los dos valores de las dos formas posibles
+division_1 = dato_1 / dato_2
+division_2= dato_2 / dato_1
+print("Resultado de la suma es: ",suma)#llamamos a la suma para que muestre el valor en este caso sera 12
+print("restulato de la resta 1: ",resta_1)#llamamos al resultado de la resta 1 que es 10-2
+print("restulato de la resta 2: ",resta_2)#llamamos al resultado de la resta 2 que es 2-10
+print("resultado de la multiplicacion: ", multiplicacion)#llamamos al resultado de la multiplicacion 
+print("resultado de la division 1: ",division_1)#llamamos al resultado de la division 1 10/2
+print("resultado de la division 2: ",division_2)#llamamos al resultado de la division 2 2/10
