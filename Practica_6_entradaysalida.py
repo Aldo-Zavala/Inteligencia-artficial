6a14e518f0bdfbcd30e9fbfa9f48d18586e18871
Aldo-Zavala <a23110123@ceti.mx>
2025-09-16 17:15:59 -0600

    Practica_6

---
+++ Practica_6_entradaysalida.py
@@ -0,0 +1,5 @@
+#En esta practica vamos capturar datos y motrsrlos en pantalla
+nombre=input("Registra tu nombre: ")#aqui le pedimos al usuario que introdusca su nombre
+edad=input("Registra tu edad: ")#aqui le pedimos al usuario que introdusca su edad
+print("Nombre: ",nombre)
+print("Edad: ",edad)
