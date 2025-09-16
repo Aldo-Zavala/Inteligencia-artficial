64e60576a0f002ed2f556a3c67c380dd9f5ccb91
Aldo-Zavala <a23110123@ceti.mx>
2025-09-16 14:26:01 -0600

    Create practica_2.py

---
+++ practica_2.py
@@ -0,0 +1,6 @@
+nombre_persona = "Aldo Zavala"#la variable nombre_presona se le asigna un valor string
+edad = 20 #declaramos la variable edad donde se le asigna un valor entero 
+print("Nombre: ")#imprimimos un texto que diga Nombre: 
+print(nombre_persona)#llamamos a la variable nombre_persona
+print("Edad: ")#imprimimos un texto que diga Edad: 
+print(edad)#llamamos a la la variable edad
