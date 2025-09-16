b9fffbebe1c4be562c99ecee273961ac80a9c4a5
Aldo-Zavala <a23110123@ceti.mx>
2025-09-16 17:16:21 -0600

    Practica_5

--- practica_5_potencias.py
+++ practica_5_potencias.py
@@ -1,5 +1,5 @@
 #En esta practica vamos a elevar un numero a auna potencai utilizando el factor de potencia "**"
-multiplicacion = 3*3*3*3*3
-potencia = 3**5
-print("el resultado de la multiplicacion 3*3*3*3*3 es : ",multiplicacion)
-print("el resultado de 3^5 utilizando 3**5 es: ",potencia)
+multiplicacion = 3*3*3*3*3#declaramos la variable mutliplicacion donde multiplicacmos 5 veces el numero 3
+potencia = 3**5#declaramos la variable potencia donde multiplicamos 5 veces el numero 3 utilizando el factor de potencia "**"
+print("el resultado de la multiplicacion 3*3*3*3*3 es : ",multiplicacion)#llamamos al resultado de la multiplicacion 
+print("el resultado de 3^5 utilizando 3**5 es: ",potencia)#llamamos al resultado de la potencia que por ende debe de ser igual al resultado de la multiplicacion
