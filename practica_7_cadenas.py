ff5b4ac3bc381d0f36a830880d5a07374853100f
Aldo-Zavala <a23110123@ceti.mx>
2025-09-18 12:07:20 -0600

    Practica_7

--- practica_7_cadenas.py
+++ practica_7_cadenas.py
@@ -1,5 +1,5 @@
-frase_1 = "hola soy aldo estudio en el ceti"
-frase_2 = "y ademas me gusta prgramar en python"
+frase_1 = "hola soy aldo estudio en el ceti"#creamos una frase
+frase_2 = "y ademas me gusta prgramar en python"#creamos otra frase
 #juntamos las dos frases 
 frase_completa= frase_1 + " "+ frase_2
-print(frase_completa)
+print(frase_completa)#mandamos llamar la frase completa
