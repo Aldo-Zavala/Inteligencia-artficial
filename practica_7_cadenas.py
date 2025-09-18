391b246742a6156eeb75df21c443495deaf1a86f
Aldo-Zavala <a23110123@ceti.mx>
2025-09-18 11:39:39 -0600

    Create practica_7_cadenas.py

---
+++ practica_7_cadenas.py
@@ -0,0 +1,5 @@
+frase_1 = "hola soy aldo estudio en el ceti"
+frase_2 = "y ademas me gusta prgramar en python"
+#juntamos las dos frases 
+frase_completa= frase_1 + " "+ frase_2
+print(frase_completa)
